35cb554fda6ab03763d8c3b59795aa613a9584e7
Swati <swatgupta375@gmail.com>
2021-03-28 14:19:38 +0530

    mean and standard deviation for each measurement

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,6 @@
 library(dplyr)
 
+
 # reading training data
 X_train <- read.table("./getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/train/X_train.txt")
 Y_train <- read.table("./getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/train/Y_train.txt")
@@ -10,7 +11,19 @@ X_test <- read.table("./getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/tes
 Y_test <- read.table("./getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/Y_test.txt")
 Subj_test <- read.table("./getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/subject_test.txt")
 
-#merging testing and training data
+# 1. merging testing and training data
 X_merg <- rbind(X_train, X_test)
 Y_merg <- rbind(Y_train, Y_test)
 Sub_merg <- rbind(Subj_train, Subj_test)
+
+
+
+# read data description
+variable_names <- read.table("./getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/features.txt")
+
+# read activity labels
+activity_labels <- read.table("./getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/activity_labels.txt")
+
+# 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+var_select <- variable_names[grep("mean\\(\\)|std\\(\\)",variable_names[,2]),]
+X_merg <- X_merg[,var_select[,1]]
